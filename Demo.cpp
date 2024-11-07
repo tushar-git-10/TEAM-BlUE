28d9bd37f9fb52559d98f09d27543bb08e045bc5
tusharkokare <96014214+tusharkokare@users.noreply.github.com>
2024-11-08 02:11:39 +0530

    Update Demo.cpp
    Tushar

--- Demo.cpp
+++ Demo.cpp
@@ -1,19 +1,9 @@
 #include <iostream>
 
-   
-    int table(int val ,int i = 1 ){
-
-       if (i==11){
-        return val;
-
-       }
-        std::cout << val*i  << std::endl;
-        table(val,++i);
-
-
-        
-    }
-
+   cout<<"Hello Brother ";
+   import pandas as pd 
+dict = {}
+   show = pd.DataFrame(dict);
 
 
 
